0d9d3a660eda62dc9a5ebd857378dc4b17697d95
saxonkidder <110950125+saxonkidder@users.noreply.github.com>
2022-11-03 23:22:29 -0500

    Delete insert_newdata.sql

--- insert_newdata.sql
+++
@@ -1,8 +0,0 @@
-COPY bzan545newdata
-FROM 'C:\\Temp\\bzan545newdata.csv' WITH (format csv, header true, delimiter ',');
-INSERT INTO bzan545masterdata
-SELECT * FROM bzan545newdata;
-
-/*truncate table bzan545masterdata*/
-/*select * from bzan545masterdata*/
-/*select * from bzan545newdata*/
